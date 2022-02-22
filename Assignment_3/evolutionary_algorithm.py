f191f1ae6ef62aeeff319facaa5f0cf8c5584e33
Thomas Bahne <thomas.bahne@gmx.net>
2022-02-22 01:01:40 +0100

    implemented lifecycle and started generational change method

--- Assignment_3/evolutionary_algorithm.py
+++ Assignment_3/evolutionary_algorithm.py
@@ -10,6 +10,18 @@ from typing import Tuple
 # def weights_to_binary(weights: np.array) -> np.array:
 #     return np.array([float_to_binary(w) for w in weights])
 
+class History():
+
+    def __init__(self) -> None:
+        self._num_generations = 0
+        self._positions = [] # each list element stores the positions of all individuals of a generation
+        self._fitness = [] # each listelement stores the fitness of all individuals of a generation
+        self._genotype_fittest = None
+
+    # TO-DO: implement function that finds the individual with the highes fitness over all generations
+
+    pass
+
 class Population():
 
     def __init__(self, num_individuals, ann_layers:Tuple[int], bias_nodes:Tuple[bool], fitness_func:func) -> None:
@@ -21,13 +33,35 @@ class Population():
         self._indiv_gene_length = helper.get_network_size(self._layers)
         self._individuals = [Individual(0, self._indiv_gene_length) for i in range(self._size)]
 
+
+    def get_all_fitness(self) -> np.ndarray:
+        ''' returns an array with all fitness values of the individuals '''
+    
+        fitness = np.array([ind.fitness for ind in self._individuals])
+        return fitness
+
+
+    def update_fitness(self, positions: np.ndarray) -> None:
+        """Updates the fitness of all individuals in the population.
+
+        Args:
+            positions (np.ndarray): Multi-dimensional array of coordinates of each individual. In case of 2D, row 1 should have x-coordinates
+                                    and row 2 should have y-coordiantes.
+        """        
+
+        assert positions.shape == (self._fit_func_dim, self._size), 'Dimensions of positions do not match fitness function dimensions and population size'
+
+        for indiv_number, individual in enumerate(self._individuals):
+            coordinates = [positions[dim][indiv_number] for dim in enumerate(self._fit_func_dim)]
+            individual.update_fitness(self._fit_func(*coordinates))
+
+
     def initial_positions(self, center:np.ndarray=None, width:float=1) -> np.ndarray:
         """Generates initial positions for individuals in a "num_dimensions"-dimensional space.
         Coordinates are drawn from a uniform random distribution centered at "center" width of intervall_length
 
         Args:
-            num_dimensions (int): number of dimensions for the coordinates to be generated
-            center (np.ndarray): (x,y) center of the uniform distribution
+            center (np.ndarray): center of the uniform distribution
             width (float): width of the uniform distribution
 
         Returns:
@@ -44,20 +78,61 @@ class Population():
         XY = np.add(XY, center_shift)
         return XY
 
+
     def lifecycle(self, time:int, get_ann_inputs:func, update_rate:float=1/50, center:np.ndarray=None, width:float=1):
-        ''' get_ann_inputs needs to be a function that takes the position of the individual as an input and returns
-            the values that should be passed as inputs to the ANN (e.g. the gradients for the benchmark functions) or
+        """Initializes ANNs according to Genotypes of the individuals and let the individuals move. After a set number of
+        iterations, the fitness of every individual is updated.
+
+        Args:
+            time (int): determines together with update_rate how many times the individuals are allowed to move
+            get_ann_inputs (func): function that takes the position of the individual as an input and returns 
+            the values that should be passed as inputs to the ANN (e.g. the gradients for the benchmark functions) or 
             the distance sensor measurements for the robot.
-            update_rate needs to be 1/XX where XX is an integer '''
+
+            update_rate (float, optional): needs to be 1/XX where XX is an integer. Defaults to 1/50.
+            center (np.ndarray, optional): see description of initial_position. Defaults to None.
+            width (float, optional): see description of initial_position. Defaults to 1.
+        """
+
+        networks = [helper.array_to_network(individual.float_genotype, self._layers, self._bias) for individual in self._individuals]
         pos = self.initial_positions(center, width)
+
         for step in range(int(time/update_rate)):
-            networks = [helper.array_to_network(individual.float_genotype, self._layers, self._bias) for individual in self._individuals]
             for i in range(self._size):
                 inputs = get_ann_inputs(pos[0][i], pos[1][i])
                 velocity = networks[i].prop_forward(inputs)
                 pos[0][i] += update_rate*velocity[0]
                 pos[1][i] += update_rate*velocity[1]
-                print(f'pos is {pos}')
+            # TO-DO: store positions in history to plot movements
+        
+        self._eval_fitness(pos)
+
+
+    def generational_change(self) -> None:
+        pass
+        # do selection
+        # do reproduction/crossover
+        # do mutation
+        # replace current population with population of the next time step
+
+    def evolution(self, num_generations:int, time_for_generation:int, get_ann_inputs:func, update_rate:float,
+                        center:np.ndarray=None, width:float=1) -> None:
+        """Performs the entire evolutionary algorithm.
+
+        Args:
+            num_generations (int): number of generations before the algorithm ends
+            time_for_generation (int): see description of "time" parameter in lifecycle
+            get_ann_inputs (func): see description of lifecycle
+            update_rate (float): see description of lifecycle
+            center (np.ndarray, optional): see description of initial_position. Defaults to None.
+            width (float, optional): see description of initial_position. Defaults to 1.
+        """
+        for i in range(num_generations):
+            self.lifecycle(time_for_generation, get_ann_inputs, update_rate, center, with)
+            self.generational_change()
+
+
+            
             
 
 class Individual():
@@ -98,11 +173,9 @@ class Individual():
 
 # General methods
 
-def get_all_fitness(individuals: np.array) -> np.array:
+def get_all_fitness(individuals: np.ndarray) -> np.ndarray:
     ''' returns an array with all fitness values of the individuals '''
-    
     fitness = np.array([ind.fitness for ind in individuals])
-
     return fitness
 
 def verbose_individuals_fitness(individuals: np.array, bin_genes=False, float_genes=False) -> None:
@@ -137,7 +210,7 @@ def chained_genotype(genes: np.array):
 
 # https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)
 
-def roulette_wheel_selection(individuals: np.array) -> np.array:
+def roulette_wheel_selection(individuals: np.ndarray) -> np.ndarray:
     '''
         Return the randomly selected individuals proportional to their FITNESS value
         Careful! One individual may be drawn multiple times
@@ -164,7 +237,7 @@ def roulette_wheel_selection(individuals: np.array) -> np.array:
 
     return selected_individuals
 
-def linear_rank_selection(individuals: np.array) -> np.array:
+def linear_rank_selection(individuals: np.ndarray) -> np.ndarray:
     '''
         Return the randomly selected individuals proportional to their RANK value
         Mostly used when the individuals in the population have very close fitness values
@@ -194,7 +267,7 @@ def linear_rank_selection(individuals: np.array) -> np.array:
 
 # Crossover
 
-def N_point_crossover(parent1: Individual, parent2: Individual, nr_points: int) -> tuple:
+def N_point_crossover(parent1: Individual, parent2: Individual, nr_points: int) -> Tuple:
     assert nr_points < 10, "crossover points are too high"
     
     # concatenate all genes
