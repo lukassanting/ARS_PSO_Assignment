671bf99bcc5bc86b25e9bb30f95ebaa00ba5c250
Lukas Santing <lukas.santing@gmail.com>
2022-03-19 20:17:21 +0100

    Now includes walls and visualised beacons. Added a main file anda robot that can move to basic extent. TODO:  - add fn to draw line to show path of robot  - add kalman filter

--- Assignment_5/pygame_classes.py
+++ Assignment_5/pygame_classes.py
@@ -2,16 +2,21 @@ import pygame
 import numpy as np
 
 class Robot:
+    """
+    Robot class for a differential drive robot in a pygame simulation. Includes
+        - a function (with supporting functions) for calculating movement, given keyboard 
+        - a function to draw the robot to a provided pygame display
+    """
     def __init__(self,
-                 pygame_display,
-                 radius,
-                 color,
-                 position,
-                 distance_between_wheels,
-                 acceleration=10,
-                 angular_acceleration=5,
-                 FPS=1/50,
-                 current_time=0):
+                 pygame_display: pygame.display,
+                 radius: int,
+                 color: tuple,
+                 position: tuple,
+                 distance_between_wheels: int,
+                 acceleration: int = 10,
+                 angular_acceleration: int = 5,
+                 FPS: float = 1 / 50,
+                 current_time: int = 0):
         self._acc = acceleration
         self._angular_acc = angular_acceleration
         self._color = color
@@ -19,11 +24,11 @@ class Robot:
         self._l = distance_between_wheels
         self._pos = position
         self._radius = radius
-        self._rot_rate = 0 # corresponds to omega in slides
-        self._rot_radius = 0 # corresponds to (uppercase) R in slides
+        self._rot_rate = 0  # corresponds to omega in slides
+        self._rot_radius = 0  # corresponds to (uppercase) R in slides
         self._start = position
         self._time = current_time
-        self._time_step_size = 1/FPS
+        self._time_step_size = 1 / FPS
         self._vel_right = 0
         self._vel_left = 0
 
@@ -39,29 +44,31 @@ class Robot:
             if key == pygame.K_x: self.stop(verbose)
             if key == pygame.K_r: self.reset(verbose)
 
-        vel_forward = np.round((self._vel_right+self._vel_left)/2, decimals=8)
-        deriv_x = (vel_forward)*np.cos(self._pos[2])
-        deriv_y = (vel_forward)*np.sin(self._pos[2])
-        deriv_theta = 0.1*(1/self._l)*(self._vel_right-self._vel_left)
-        self._pos = self._pos + time_elapsed*np.array([deriv_x, deriv_y, deriv_theta])
+        vel_forward = np.round((self._vel_right + self._vel_left) / 2, decimals=8)
+        deriv_x = (vel_forward) * np.cos(self._pos[2])
+        deriv_y = (vel_forward) * np.sin(self._pos[2])
+        deriv_theta = 0.1 * (1 / self._l) * (self._vel_right - self._vel_left)
+        self._pos = self._pos + time_elapsed * np.array([deriv_x, deriv_y, deriv_theta])
 
         self._time += time_elapsed
 
     def accelerate(self, verbose=False):
-        self._vel_right = np.round(self._vel_right+self._acc, decimals=8)
-        self._vel_left = np.round(self._vel_left+self._acc, decimals=8)
+        self._vel_right = np.round(self._vel_right + self._acc, decimals=8)
+        self._vel_left = np.round(self._vel_left + self._acc, decimals=8)
         self.update_rotate_rate()
         self.update_rotate_radius(verbose)
         if verbose:
-            print(f'Accelerating right: {self._vel_right}, Accelerating left: {self._vel_left}, rotation rate: {self._rot_rate}, rotation radius: {self._rot_radius}')
+            print(
+                f'Accelerating right: {self._vel_right}, Accelerating left: {self._vel_left}, rotation rate: {self._rot_rate}, rotation radius: {self._rot_radius}')
 
     def decelerate(self, verbose=False):
-        self._vel_right = np.round(self._vel_right-self._acc, decimals=8)
-        self._vel_left = np.round(self._vel_left-self._acc, decimals=8)
+        self._vel_right = np.round(self._vel_right - self._acc, decimals=8)
+        self._vel_left = np.round(self._vel_left - self._acc, decimals=8)
         self.update_rotate_rate()
         self.update_rotate_radius(verbose)
         if verbose:
-            print(f'Decelerating right: {self._vel_right}, Decelerating left: {self._vel_left}, rotation rate: {self._rot_rate}, rotation radius: {self._rot_radius}')
+            print(
+                f'Decelerating right: {self._vel_right}, Decelerating left: {self._vel_left}, rotation rate: {self._rot_rate}, rotation radius: {self._rot_radius}')
 
     def increase_angle(self, verbose=False):
         self._pos[2] += self._angular_acc
@@ -88,17 +95,17 @@ class Robot:
             print(f'Position of robot has been reset to the initial starting position of {self._start}')
 
     def update_rotate_rate(self):
-        self._rot_rate = np.round(((self._vel_right - self._vel_left)/self._l), decimals=8)
+        self._rot_rate = np.round(((self._vel_right - self._vel_left) / self._l), decimals=8)
 
     def update_rotate_radius(self, verbose=False):
         if self._vel_right == self._vel_left:
             self._rot_radius = np.Inf
         elif self._vel_right == 0:
-            self._rot_radius = -1*np.round(self._l/2, decimals=8)
-        elif self._vel_left==0:
-            self._rot_radius = np.round(self._l/2, decimals=8)
+            self._rot_radius = -1 * np.round(self._l / 2, decimals=8)
+        elif self._vel_left == 0:
+            self._rot_radius = np.round(self._l / 2, decimals=8)
         else:
-            self._rot_radius = np.round((self._vel_right - self._vel_left)/self._l, decimals=8)
+            self._rot_radius = np.round((self._vel_right - self._vel_left) / self._l, decimals=8)
 
         if verbose:
             print(f'Updated R: {self._rot_radius}')
